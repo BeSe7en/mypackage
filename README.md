7b1a79bbb78bc0ee3c55b8c195973fdd47555ce9
Bongokuhle Dladla <thabobongokuhle@gmail.com>
2023-03-12 16:04:50 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#mypackage
+this libary was created as an example to publish your own Python package
+
+#How to instal
+....
---
+++ mypackage/myModule.py
@@ -0,0 +1,28 @@
+def top_n (items,n):
+    """ Return the top n items in an array, in desc order.
+
+    args:
+        items(array): list or array-like object
+        n(int): nums of items to return 
+
+    Return:
+        array: top n items, in desc order
+
+    eggs:
+       >>> top_n([8,3,2,7,4])
+       [8,7,4]
+    """
+    for i in range(n):#keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1]:#if this item is bigger than next item...
+                items[j],items[j+1] = items[j+1],items[j]#sawp the two!
+
+    #get last two items
+    top_n = items[-n:]
+
+    #return in descerding order
+    return top_n(::-1)
+        
+
+   
